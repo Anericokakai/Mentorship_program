2dc2800ad2a798b3500e9bbf116c603a84b5536a
Festus <festusgitahik@gmail.com>
2023-07-05 00:20:29 +0300

    created chat page

---
+++ client/src/pages/chats/components/Search.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Search() {
+  return (
+    <div className="search">
+      <div className="user-seach">
+        <input type="text" placeholder="search for a chat" />
+      </div>
+
+      
+    </div>
+  );
+}
+
+export default Search;
