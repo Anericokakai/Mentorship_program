8164b4576fc7fce09e7ee3615f14b2effacc33a4
Anericokakai <challykiddoh@gmail.com>
2023-07-07 15:55:22 +0300

    fixed login ui

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -27,6 +27,12 @@ function Signup() {
   const [show, setshow] = useState(false);
   const [show2, setshow2] = useState(false);
   const [loading, setloading] = useState();
+  const [strong, setstrong] = useState();
+  const [weak, setweak] = useState();
+  const [focused, setfocused] = useState();
+  const [medium, setmedium] = useState();
+  // !strength of the password
+  const [checkpass, setcheckpass] = useState("");
   // !change between sign up and login forms
   function handleChangeForm() {
     if (login === false) {
@@ -37,9 +43,9 @@ function Signup() {
   }
   // ! redirect function
   // todo REUSABLES
-  const redirect=()=>{
-    return window.location.href='/'
-  }
+  const redirect = () => {
+    return (window.location.href = "/");
+  };
 
   // !handle change of form
   const handleForm = async (e) => {
@@ -50,7 +56,7 @@ function Signup() {
       await handleformSubmitForSignUp(formsInputs)
         .then((response) => {
           // !dismiss loafind
-   setloading(false)
+          setloading(false);
           if (response.data.error) {
             toast.error(response.data.message);
           } else {
@@ -61,12 +67,12 @@ function Signup() {
         })
         .catch((error) => {
           toast.error(error);
-          setloading(false)
+          setloading(false);
         });
     } else if (login) {
       await handleLoginForStudents(formsInputs)
         .then((response) => {
-          setloading(false)
+          setloading(false);
           console.log(response.data);
           if (response.data.error === true) {
             toast.error(response.data.message);
@@ -76,20 +82,20 @@ function Signup() {
             const userToken = response.data.token;
             const refresh = response.data.refreshToken;
             const givenRole = response.data.role;
-            const id=response.data.id
+            const id = response.data.id;
             dispatch(Settoken(userToken));
             dispatch(Setrefreshtoken(refresh));
             dispatch(Setroles(givenRole));
-            dispatch(setId(id))
+            dispatch(setId(id));
 
             formsInputs.reset();
 
-            setTimeout(redirect,4000)
+            setTimeout(redirect, 4000);
           }
         })
         .catch((error) => {
           toast.error(error);
-          setloading(false)
+          setloading(false);
         });
     }
   };
@@ -101,8 +107,8 @@ function Signup() {
     } else {
       setshow(false);
     }
-    console.log(inputType_password)
-      inputType_password && showPassword(inputType_password);
+    console.log(inputType_password);
+    inputType_password && showPassword(inputType_password);
   };
   const showOrHideCon = () => {
     if (show2 === false) {
@@ -114,6 +120,39 @@ function Signup() {
     showPassword(inputCon);
   };
 
+  // !handle change in passowrd
+
+  const handleChange = (e) => {
+    const val = e.target.value;
+    setcheckpass(val);
+
+   if( val!='') {
+    setweak(true);
+   } else{
+    setweak(false)
+   }
+    // !reg expresion to check password
+    const strongpassword = new RegExp(
+      "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})"
+    );
+    const mediumPass = new RegExp(
+      "((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))"
+    );
+    if (strongpassword.test(val)) {
+      setstrong(true);
+      setweak(false);
+      setmedium(false)
+    } else if (mediumPass.test(val)) {
+     
+      setmedium(true)
+      setweak(false);
+      setstrong(false)
+    }else{
+      setmedium(false)
+      setstrong(false)
+    }
+  };
+ 
   return (
     <div>
       <ToastContainer
@@ -126,9 +165,7 @@ function Signup() {
       />
       <div className="signupcontainer">
         <div className="singupForm">
-          {loading && (
-           <Preloader></Preloader>
-          )}
+          {loading && <Preloader></Preloader>}
           <h1>{login ? "sign in to Mentors.io" : "sign up  to mentors.io"}</h1>
           <form action="" className="Form1" onSubmit={handleForm}>
             {login === false && (
@@ -156,12 +193,25 @@ function Signup() {
                 placeholder="password"
                 name="password"
                 className="password"
+                value={checkpass}
+                onFocus={() => setfocused(true)}
+                onBlur={() => setfocused(false)}
+                onChange={(e) => handleChange(e)}
               />
               {show ? (
                 <i class="fa-regular fa-eye-slash" onClick={showOrHide}></i>
               ) : (
                 <i class="fa-regular fa-eye" onClick={showOrHide}></i>
               )}
+              {!login && focused && strong && (
+                <p className='strong message'>strong password <i class="fa-solid fa-check-double"></i> </p>
+              )}
+              {!login && focused && weak && (
+                <p className='red message'>weak password <i class="fa-solid fa-x"></i> </p>
+              )}
+              {
+               !login && focused && medium &&  <p className= "medium message">medium password  <i class="fa-solid fa-check"></i> </p>
+              }
             </div>
             {login === false && (
               <div className="inputs">
@@ -183,7 +233,7 @@ function Signup() {
             )}
 
             <div className="inputs">
-              <button  type="submit" className="btn btnsign">
+              <button type="submit" className="btn btnsign">
                 {login ? "sign in" : " sign up"}
               </button>
             </div>
